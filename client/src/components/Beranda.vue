0e6de4d17ff6b695ff467b02b091dac05b7f4d95
hacktiv8 <aguspermadi94@gmail.com>
2017-04-20 14:32:43 +0700

    get articles

---
+++ client/src/components/Beranda.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <div class="beranda">
+    <h3>Welcome Home</h3>
+    <div class="list-article">
+      <ul v-for="article in articles">
+        <li> {{ article.title }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      articles : []
+    }
+  },
+  methods: {
+    getArticles() {
+      let self = this
+      axios.get('http://localhost:3000/articles')
+            .then(function(res) {
+              // console.log(res);
+              self.articles.push(res.data)
+            }).catch(function(err) {
+              console.log(err);
+            })
+            console.log(self.articles);
+    }
+  },
+  mounted() {
+    this.getArticles()
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="navbar">
+    <el-menu theme="light" class="el-menu-demo" mode="horizontal">
+      <router-link to="/"><el-menu-item index="1">Beranda</el-menu-item></router-link>
+      <router-link to="/about"><el-menu-item index="2">About</el-menu-item></router-link>
+      <router-link to="/list"><el-menu-item index="2">List</el-menu-item></router-link>
+      <router-link to="/login"><el-menu-item style="float: right;" index="3">Login</el-menu-item></router-link>
+      <router-link to="/user"><el-menu-item style="float: right;" index="3">Profil</el-menu-item></router-link>
+      <router-link to="/logout"><el-menu-item style="float: right;" index="3">Logout</el-menu-item></router-link>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
